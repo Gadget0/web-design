f4779727e77bb854bfdc799e1c650fecbdd76200
Gadget0 <319262687@qq.com>
2023-01-30 20:23:13 +0800

    “第二次”

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
-    <div class="wd-top">顶部导航栏</div>
-    <div class="wd-content"></div>
-    <div class="wd-bottom"></div>
+    <!-- <div class="wd-top">顶部导航栏111</div> -->
+    <div class="wd-con">
+      <div class="con-top">top</div>
+      <div class="con-con">con
+        <div class="con-con-top">
+          <ul>
+            <li>首页</li>
+            <li>机构设置</li>
+            <li>重要文件</li>
+            <li>组织民管</li>
+            <li>保障女工</li>
+            <li>生产宣传</li>
+            <li>财务经审</li>
+            <li>综合办公</li>
+            <li>党组织建设</li>
+          </ul>
+        </div>
+      </div>
+      <div class="con-button">button</div>
+    </div>
+    <div class="wd-bottom">bottom</div>
     <div class="wd-sider">
       <div class="wd-sider-list">内容1</div>
       <div class="wd-sider-list">内容2</div>
@@ -11,10 +29,8 @@
       <div class="wd-sider-list">内容5</div>
     </div>
     <div class="wd-back-top" v-if="showBtn" @click="backTop()">回顶部</div>
-    
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
@@ -26,7 +42,6 @@ export default {
   },
   mounted() {
     window.addEventListener("scroll", this.scrollEvent, true);
-
   },
   methods: {
     // 回到顶部是否狮子
@@ -44,7 +59,6 @@ export default {
         this.showBtn = false;
       }
       let header = document.querySelector('.wd-top')
-      
       // let styTop = document.querySelector('.wd-top')
       if (that.scrollTop > 60) {
         header.style.background = 'red'
@@ -54,14 +68,12 @@ export default {
         header.style.top = '-90px'
       }
     },
-
     /*回到顶部实现过程注解：
-        1.获取当前页面距离顶部的距离
-        2.计算出每次向上移动的距离，用负的滚动距离除以5，因为滚动的距离是一个正数，想向上移动就是一个减法
-        3.用当前距离加上计算出的距离，然后赋值给当前距离，就可以达到向上移动的效果
-        4.最后在移动到顶部的时候，清除定时器
-        */
-
+      1.获取当前页面距离顶部的距离
+      2.计算出每次向上移动的距离，用负的滚动距离除以5，因为滚动的距离是一个正数，想向上移动就是一个减法
+      3.用当前距离加上计算出的距离，然后赋值给当前距离，就可以达到向上移动的效果
+      4.最后在移动到顶部的时候，清除定时器
+    */
     backTop() {
       var timer = setInterval(function () {
         let osTop =
@@ -78,9 +90,8 @@ export default {
   }  
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" scoped>
 .wd-before-top{
   width: 100%;
   height: 100px;
@@ -103,13 +114,12 @@ export default {
 .wd-sider {
   width: 80px;
   position: fixed;
-  right: 4%;
+  right: 1%;
   top: 45%;
   text-align: center;
   line-height: 35px;
   font-size: 25px;
   background-color: rgb(54, 87, 233);
-  
   box-shadow: 0px 1px 3px 1px #888888;
   z-index: 999;
 }
@@ -135,11 +145,25 @@ export default {
    box-shadow: 0px 1px 3px 1px #888888;
    z-index: 999;
  }
-
- 
-.wd-content{
+.wd-con{
   width: 100%;
-  height: 10000px;
-  background: pink;
+  // height: 10000px;
+  background: white;
+  border: 1px solid red;
+  .con-top {
+    height: 20vh;
+    background: goldenrod;
+    border: 1px solid gold;
+  }
+  .con-con{
+    height: 40vh;
+    background: gray;
+    border: 1px solid gray;
+  }
+  .con-button{
+    height: 20vh;
+    background: bisque;
+    border: 1px solid gray;
+  }
 }
 </style>
